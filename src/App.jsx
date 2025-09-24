318786a7d17aa8f1edc2410d56480d048fae578a
Dev-Baskar <baskar13008@gmail.com>
2025-09-24 12:34:09 +0530

    feat : update Wealth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Wealth from "./components/wealth/wealth";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/wealth" component={Wealth} />
       </Switch>
     </>
   );
