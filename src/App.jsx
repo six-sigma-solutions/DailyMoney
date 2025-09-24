f71847a70dbbe5e418cbe80a554f5c3ef3cae433
Sathish M <sathish19222978sk@gmail.com>
2025-09-24 13:14:08 +0530

    updated wealth section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import CoreTeam from "./components/coreTeam/CoreTeam";
+import Wealth from "./components/wealth/wealth";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/core-team" component={CoreTeam} />
+        <Route exact path="/wealth" component={Wealth} />
       </Switch>
     </>
   );
