8603ac350adf5b9b0cb89107f3640a341036daa0
Sathish M <sathish19222978sk@gmail.com>
2025-09-30 23:27:42 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./components/home/Home";
 import Wealth from "./components/wealth/Wealth";
 import Health from "./components/health/Health";
 import Family from "./components/family/Family";
