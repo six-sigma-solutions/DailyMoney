82e05561c79055c9d612998b14073a62550f6333
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-04 17:56:55 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
                 <Link id="tab-name" to="/overview">
                   ▾ Overview
                 </Link>
-                <Link id="tab-name" to="/visionmission">
+                <Link id="tab-name" to="/vision-mission">
                   ▾ Vision & Mission
                 </Link>
                 <Link id="tab-name" to="/founders-msg">
