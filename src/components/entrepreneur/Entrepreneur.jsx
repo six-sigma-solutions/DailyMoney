d4a1c2310727999251c52fefa80aef5f7ebc2500
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-10 08:31:21 +0530

    Update Entrepreneur.jsx

--- src/components/entrepreneur/Entrepreneur.jsx
+++ src/components/entrepreneur/Entrepreneur.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // !! IMPORTANT: Make sure these paths are correct for your project !!
-import img1 from "/Enter1.jpeg";
+import img1 from "/Enter11.jpeg";
 import img3 from "/img-3.jpg";
 import img4 from "/img-4.jpg";
 import img5 from "/img-5.jpg";
@@ -21,7 +21,7 @@ export default function Entrepreneur () {
       <div className="ent-section">
         <h1 className="entre-title">Empowering You to Lead, Build, and Grow</h1>
         <div className="ent-title">
-          <img src={img1} alt="Entrepreneur" />
+          <img src={img11} alt="Entrepreneur" />
         </div>
       </div>
 
