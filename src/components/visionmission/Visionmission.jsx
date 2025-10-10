6f0f55305a41b32d0115ad92a4a8b4bd20b1073a
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-10 12:47:40 +0530

    Update Visionmission.jsx

--- src/components/visionmission/Visionmission.jsx
+++ src/components/visionmission/Visionmission.jsx
@@ -158,7 +158,7 @@ const Visionmission = () => {
         <div className="vision-gallery">
           <img src="vmd-1-g.jpeg" alt="Vision gallery 1" />
           <img src="vmd-22-g.jpeg" alt="Vision gallery 2" />
-          <img src="vmd-3-g.jpeg" alt="Vision gallery 3" />
+          <img src="cv-58.jpg" alt="Vision gallery 3" />
           <img src="vmd-44-g.jpeg" alt="Vision gallery 4" />
         </div>
       </section>
