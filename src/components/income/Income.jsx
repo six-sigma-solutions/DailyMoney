d8bac82e2a723b86d967d8ae0136a56200ccdbc9
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-11 10:44:09 +0530

    Update Income.jsx

--- src/components/income/Income.jsx
+++ src/components/income/Income.jsx
@@ -35,7 +35,7 @@ const Income = () => {
           <p>
             True income, however, is not just financial. It is about the returns
             you gain from life itself — the knowledge you acquire, the
-            relationships you nurture, and the health you sustain.
+            relationships you nurture, and the health you maintain.
           </p>
         </div>
         <div className="income-image">
