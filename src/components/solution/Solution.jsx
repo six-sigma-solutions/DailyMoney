a154efe542a72aad2ad3906d87fea787028044fd
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-11 15:33:17 +0530

    Update Solution.jsx

--- src/components/solution/Solution.jsx
+++ src/components/solution/Solution.jsx
@@ -36,7 +36,7 @@ const Solution = () => {
           <div className="dm-sol-img-card">
           <img
             className="dm-sol-card-media"
-            src="/solution-455.jpg"
+            src="/copy.jpg"
             alt="Solution"
           />
          
