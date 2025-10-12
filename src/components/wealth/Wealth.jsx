5c801ff5d02edc4ec3761ce9ab6bc2d1eef139dc
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-12 10:50:41 +0530

    Update Wealth.jsx

--- src/components/wealth/Wealth.jsx
+++ src/components/wealth/Wealth.jsx
@@ -32,7 +32,7 @@ const Wealth = () => {
           {/* Row 1 */}
           <div className="wealthy-row">
             <div className="wealthy-col wealthy-image-col">
-              <img src="mo.jpg" alt="Wealth concept 1" />
+              <img src="hhh.jpg" alt="Wealth concept 1" />
             </div>
             <div className="wealthy-col wealthy-text-col row1">
               <h2>
