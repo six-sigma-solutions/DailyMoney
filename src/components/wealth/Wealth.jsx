99b79f87dbc580785c700b3f1b74469f5c9e11c4
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-14 10:51:58 +0530

    Update Wealth.jsx

--- src/components/wealth/Wealth.jsx
+++ src/components/wealth/Wealth.jsx
@@ -21,7 +21,7 @@ const Wealth = () => {
           <div className="wealthy-hero-grid">
             <div className="wealthy-hero-image" aria-hidden="true">
               <div className="wealthy-portrait-card">
-                <img src="lll.jpg" alt="Portrait" />
+                <img src="wealth000.jpg" alt="Portrait" />
               </div>
             </div>
           </div>
