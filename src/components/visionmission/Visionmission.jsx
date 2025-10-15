8a4543a6a207eb4d6196ffc3c4c6c993acf50930
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-15 09:56:04 +0530

    Update Visionmission.jsx

--- src/components/visionmission/Visionmission.jsx
+++ src/components/visionmission/Visionmission.jsx
@@ -64,7 +64,7 @@ const Visionmission = () => {
     {/* Section 1: Image Left, Text Right */}
     <div className="vision-row">
       <div className="vision-col vision-image-col">
-        <img src="vmd-1.jpeg" alt="Wealth concept 1" />
+        <img src="vm-1.jpg" alt="Wealth concept 1" />
       </div>
       <div className="vision-col vision-text-col">
         <h2>
