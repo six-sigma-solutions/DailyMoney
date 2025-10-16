956c6b95eff62e83413ce05c5572ee14f8dac1a9
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-16 21:26:21 +0530

    Update Visionmission.jsx

--- src/components/visionmission/Visionmission.jsx
+++ src/components/visionmission/Visionmission.jsx
@@ -154,6 +154,17 @@ const Visionmission = () => {
 
 </section>
 
+      {/* Horizontal 4-Image Section */}
+      <section className="vision-gallery-section">
+        <h2 className="vision-gallery-title">Core Values</h2>
+        <div className="vision-gallery">
+          <img src="test1a.jpg" alt="Vision gallery 1" />
+          <img src="test22.jpg" alt="Vision gallery 2" />
+          <img src="test3.jpg" alt="Vision gallery 3" />
+          <img src="test4.jpg" alt="Vision gallery 4" />
+        </div>
+      </section>
+
 
 
 
