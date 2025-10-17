a8823a4c37d6c67f165f69f153e5bb808c401f47
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-17 11:59:22 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -182,7 +182,7 @@ export default function Home() {
               </div>
               <img
                 className="solution-card-img1"
-                src="/virat.jpg"
+                src="/virat2.png"
                 alt="AI Tools"
               />
             </div>
