64e8b3ec6507c7ab504732279c081f309e3e2b14
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-10-23 11:14:06 +0530

    Update FounderMsg.jsx

--- src/components/founderMsg/FounderMsg.jsx
+++ src/components/founderMsg/FounderMsg.jsx
@@ -62,7 +62,6 @@ export default function FounderMsg() {
               className="footer-logo"
             />
           </div>
-          <h1 className="footer-title2">DM</h1>
           <p className="footer-subtitle2">Independent for Entire Life</p>
         </div>
       </div>
