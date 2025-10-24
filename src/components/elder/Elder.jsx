a3824819f9d259a5fef0c6089ea6f3937f8842cb
dhanush200322 <146175199+dhanush200322@users.noreply.github.com>
2025-10-24 17:16:34 +0530

    Update Elder.jsx

--- src/components/elder/Elder.jsx
+++ src/components/elder/Elder.jsx
@@ -73,7 +73,7 @@ export default function Elder() {
         {/* Row 2 */}
         <section className="dm-eldr-row reverse">
           <div className="dm-eldr-poster">
-            <img src="/elder300.png" alt="Poster 2" />
+            <img src="/elders10.png" alt="Poster 2" />
           </div>
           <div className="dm-eldr-content">
             <p className="dm-eldr-animate dm-eldr-tagline">
