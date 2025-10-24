8b11d3247640521eea12403950715294f6b97f73
dhanush200322 <146175199+dhanush200322@users.noreply.github.com>
2025-10-24 17:38:24 +0530

    Update Overview.jsx

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -90,9 +90,7 @@ const Overview = () => {
         <div className="zigzag-text-content">
           <h2 style={{ color: "cyan" }}>Our Promise</h2>
           <p>
-            DM is not define about just Health, but about wealth for
-            life and beyond. We help build strong foundations so every
-            generation thrives — Healthy, financially, physically, and spiritually.
+           DM defines not only good health but lasting prosperity — helping every generation live fully, grow stronger, and flourish in all dimensions of life.
           </p>
         </div>
       </section>
