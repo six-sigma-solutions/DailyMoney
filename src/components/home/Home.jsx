b653e591e83a954596ce5159719b2687ba851146
dhanush200322 <146175199+dhanush200322@users.noreply.github.com>
2025-10-25 09:44:18 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
       
       
    <div className="dm">
-          <span>DM - Daily Message - Health and Freedom.</span>
+          <span>DM - Daily Message - Health and Freedom - The World Premium Healthy and Happy Community.</span>
         </div>
       <section className="hero-section">
      
