dc65dfee33d25b991166ab432d7e5a9c87c02d9e
Dhanush AV22 <146175199+dhanush200322@users.noreply.github.com>
2025-10-28 10:00:44 +0530

    Update CoreTeam.jsx

--- src/components/coreTeam/CoreTeam.jsx
+++ src/components/coreTeam/CoreTeam.jsx
@@ -7,7 +7,7 @@ import Footer from '../footer/Footer';
 const topMember = {
     name: "Dr.V.Chellapondy",
     role: "Financial & Business Advisor",
-    image: "cp.jpg",
+    image: "cpsp.jpg",
 
 };
 
