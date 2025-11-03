2305551ac59c6d3af4a56aa3a37ffc33c608d8a9
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-11-03 14:36:26 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -47,7 +47,7 @@ export default function Home() {
 
       <div className="dm">
         <span>
-          DM - Daily Wealth - Health and Wellness - The World Premium Healthy and
+          DM - Daily Message - Health and Wellness - The World Premium Healthy and
           Happy Community.
         </span>
       </div>
