df84ecb67f26008bbb84dc7d5e2ca125ec84c1dc
Dhanush AV22 <146175199+dhanush200322@users.noreply.github.com>
2025-11-03 11:35:20 +0530

    Update FounderMsg.jsx

--- src/components/founderMsg/FounderMsg.jsx
+++ src/components/founderMsg/FounderMsg.jsx
@@ -7,7 +7,7 @@ const founder = {
   name: "Dr.V.Chellapondy",
   role: "Founder & CEO",
   // Use the image URL you intend to use for the founder
-  image: "/CPSIR.png",
+  image: "/cpdian.png",
 };
 
 export default function FounderMsg() {
