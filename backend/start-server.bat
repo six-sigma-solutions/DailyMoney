b81031811ff95109b7e1d01b037ebbe7d211a30e
six-sigma-solutions <sixsigmaspvs@gmail.com>
2025-11-04 11:12:48 +0530

    .

--- backend/start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting DailyMoney Backend Server...
-cd /d "c:\Users\cpdia\Desktop\DailyM\DailyMoney\backend"
-echo.
-echo Environment: Production
-echo Database: dailymoney_contacts
-echo Port: 4000
-echo.
-node index.js
