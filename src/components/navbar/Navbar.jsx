fe5c8b83a3617f37cdf4af98b8cb689fd188a214
Dhanush AV22 <146175199+dhanush200322@users.noreply.github.com>
2025-11-06 14:11:00 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
         {/* LEFT: Logo */}
         <Link to="/" className="logo">
           <img
-            src="DM-99.jpg" 
+            src="DM-100.jpg" 
             alt="DailyMoney Logo"
           />
         </Link>
