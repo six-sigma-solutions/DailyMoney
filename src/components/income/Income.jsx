8d9f23730523332b4a9804d8f98a4a2f527d99bb
Your Name <your_email@example.com>
2025-11-14 11:34:26 +0530

    Initial project upload

--- src/components/income/Income.jsx
+++ src/components/income/Income.jsx
@@ -23,7 +23,7 @@ const Income = () => {
         <div className="income-image">
           <img
             className="responsive-income-img"
-            src="incomepls.png"
+            src="income400.jpg"
             alt="Income concept"
           />
         </div>
